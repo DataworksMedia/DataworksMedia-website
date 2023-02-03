0c038e29e6c6be66c72f46313695567a7f9d4a53
Andrew Rodriguez <andrew.rodriguez@dataworksmedia.com>
2023-02-02 22:36:18 -0700

    Fixing title and Fav

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Services from './components/Services';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>DataworksMedia</title>
+        <link rel="canonical" href="http://dataworksmedia.com" />
+        <meta
+          name="description"
+          content="Dataworks Media is digital marketing agency focused on Paid Advertising PPC, Content Creation, Website building and hosting, and SEO management"
+        />
+      </Helmet>
       <Navbar />
       <Hero />
       <About />
