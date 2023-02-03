7768a428359f4f5cc354d2c0a27f03e4ceeeda99
Andrew Rodriguez <andrew.rodriguez@dataworksmedia.com>
2023-02-03 00:20:50 -0700

    Fixing success page

---
+++ src/Success.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Success = () => {
+  return <p>thanks you for your submission</p>;
+};
+
+export default Success;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,7 +20,8 @@ const Contact = () => {
           </h1>
           <form
             name="contact"
-            action="/contact"
+            action="/Success/" 
+            onSubmit="submit"
             method="post"
             data-netlify="true"
           >
